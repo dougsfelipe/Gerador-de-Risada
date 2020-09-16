937a4b2d42d025c11417be4e352382422a46bd70
dougsfelipe <dfcs@cin.ufpe.br>
2020-09-15 21:06:37 -0300

    init 2

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-const alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','x','z','w','1','2','3','4','5','6','7','8','9']
+const alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'x', 'z', 'w', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
- teste = '';
+teste = '';
 
 console.log(teste)
 console.log(alfabeto)
 
 
 for (let index = 0; index < 8; index++) {
-    
-  teste = teste + alfabeto[getRandomInt(0,alfabeto.length-1)];
-    
+
+    if (alfabeto[getRandomInt(0, alfabeto.length - 1) != undefined) {
+        teste = teste + alfabeto[getRandomInt(0, alfabeto.length - 1)];
+    }
 }
 
 console.log(teste)
